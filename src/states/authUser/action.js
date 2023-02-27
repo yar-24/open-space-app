996be33846ca330ca3c3b96d6e7fb3bd8bbcb681
yar-24 <akhyar24.354@gmail.com>
2023-02-27 10:34:49 +0700

    open-ewb

---
+++ src/states/authUser/action.js
@@ -0,0 +1,59 @@
+/**
+ * @TODO: Define all the actions (creator) for the authUser state
+ */
+
+import api from '../../utils/api';
+
+const ActionType = {
+  SET_AUTH_USER: 'SET_AUTH_USER',
+  UNSET_AUTH_USER: 'UNSET_AUTH_USER',
+};
+
+function setAuthUserActionCreator(authUser) {
+  return {
+    type: ActionType.SET_AUTH_USER,
+    payload: {
+      authUser,
+    },
+  };
+}
+
+function unsetAuthUserActionCreator() {
+  return {
+    type: ActionType.UNSET_AUTH_USER,
+    payload: {
+      authUser: null,
+    },
+  };
+}
+
+// fungsi thunk bernama asyncSetAuthUser()untuk digunakan pada proses login.
+function asyncSetAuthUser({ id, password }) {
+  return async (dispatch) => {
+    try {
+      const token = await api.login({ id, password });
+      api.putAccessToken(token);
+      const authUser = await api.getOwnProfile();
+
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+//   fungsi thunk untuk menangani proses logout
+function asyncUnsetAuthUser() {
+  return (dispatch) => {
+    dispatch(unsetAuthUserActionCreator());
+    api.putAccessToken('');
+  };
+}
+
+export {
+  ActionType,
+  setAuthUserActionCreator,
+  unsetAuthUserActionCreator,
+  asyncSetAuthUser,
+  asyncUnsetAuthUser,
+};
